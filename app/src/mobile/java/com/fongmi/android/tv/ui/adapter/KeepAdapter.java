7a1680cc9a32cbf9a67cb2bad000d9e1e8ace604
jhengazuki <jhengazuki3@gmail.com>
2025-08-23 21:01:20 +0800

    Update ui

--- app/src/mobile/java/com/fongmi/android/tv/ui/adapter/KeepAdapter.java
+++ app/src/mobile/java/com/fongmi/android/tv/ui/adapter/KeepAdapter.java
@@ -78,7 +78,7 @@ public class KeepAdapter extends RecyclerView.Adapter<KeepAdapter.ViewHolder> {
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         ViewHolder holder = new ViewHolder(AdapterVodBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
-        holder.binding.image.getLayoutParams().width = width;
+        holder.binding.getRoot().getLayoutParams().width = width;
         holder.binding.image.getLayoutParams().height = height;
         return holder;
     }
