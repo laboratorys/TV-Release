bcaa729557af159fa7e6a7d0ccf06bc47e48aa4b
jhengazuki <jhengazuki3@gmail.com>
2025-09-14 22:00:08 +0800

    Update keep and history ui

--- app/src/mobile/java/com/fongmi/android/tv/ui/activity/KeepActivity.java
+++ app/src/mobile/java/com/fongmi/android/tv/ui/activity/KeepActivity.java
@@ -3,8 +3,12 @@ package com.fongmi.android.tv.ui.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.viewbinding.ViewBinding;
 
@@ -41,19 +45,19 @@ public class KeepActivity extends BaseActivity implements KeepAdapter.OnClickLis
 
     @Override
     protected void initView(Bundle savedInstanceState) {
+        setSupportActionBar(mBinding.toolbar);
         setRecyclerView();
         getKeep();
     }
 
     @Override
-    protected void initEvent() {
-        mBinding.sync.setOnClickListener(this::onSync);
-        mBinding.delete.setOnClickListener(this::onDelete);
+    public void setSupportActionBar(@Nullable Toolbar toolbar) {
+        super.setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     private void setRecyclerView() {
         mBinding.recycler.setHasFixedSize(true);
-        mBinding.recycler.getItemAnimator().setChangeDuration(0);
         mBinding.recycler.setLayoutManager(new GridLayoutManager(this, Product.getColumn(this)));
         mBinding.recycler.setAdapter(mAdapter = new KeepAdapter(this));
         mAdapter.setSize(Product.getSpec(getActivity()));
@@ -61,20 +65,17 @@ public class KeepActivity extends BaseActivity implements KeepAdapter.OnClickLis
 
     private void getKeep() {
         mAdapter.addAll(Keep.getVod());
-        mBinding.delete.setVisibility(mAdapter.getItemCount() > 0 ? View.VISIBLE : View.GONE);
     }
 
-    private void onSync(View view) {
+    private void onSync() {
         SyncDialog.create().keep().show(this);
     }
 
-    private void onDelete(View view) {
+    private void onDelete() {
         if (mAdapter.isDelete()) {
             new MaterialAlertDialogBuilder(this).setTitle(R.string.dialog_delete_record).setMessage(R.string.dialog_delete_keep).setNegativeButton(R.string.dialog_negative, null).setPositiveButton(R.string.dialog_positive, (dialog, which) -> mAdapter.clear()).show();
         } else if (mAdapter.getItemCount() > 0) {
             mAdapter.setDelete(true);
-        } else {
-            mBinding.delete.setVisibility(View.GONE);
         }
     }
 
@@ -110,15 +111,27 @@ public class KeepActivity extends BaseActivity implements KeepAdapter.OnClickLis
     @Override
     public void onItemDelete(Keep item) {
         mAdapter.remove(item.delete());
-        if (mAdapter.getItemCount() > 0) return;
-        mBinding.delete.setVisibility(View.GONE);
-        mAdapter.setDelete(false);
+        if (mAdapter.getItemCount() == 0) mAdapter.setDelete(false);
     }
 
     @Override
     public boolean onLongClick() {
         mAdapter.setDelete(!mAdapter.isDelete());
         return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_keep, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) onBackInvoked();
+        else if (item.getItemId() == R.id.delete) onDelete();
+        else if (item.getItemId() == R.id.sync) onSync();
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
