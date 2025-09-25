ac63ec86b84cc6b6724092486d07d8320e286ca8
jhengazuki <jhengazuki3@gmail.com>
2025-09-25 20:14:58 +0800

    Clean ui

--- app/src/leanback/java/com/fongmi/android/tv/ui/activity/CastActivity.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/activity/CastActivity.java
@@ -239,14 +239,14 @@ public class CastActivity extends BaseActivity implements CustomKeyDownVod.Liste
     }
 
     private void showInfo() {
-        mBinding.widget.info.setVisibility(View.VISIBLE);
+        mBinding.widget.top.setVisibility(View.VISIBLE);
         mBinding.widget.center.setVisibility(View.VISIBLE);
         mBinding.widget.exoDuration.setText(mPlayers.getDurationTime());
         mBinding.widget.exoPosition.setText(mPlayers.getPositionTime(0));
     }
 
     private void hideInfo() {
-        mBinding.widget.info.setVisibility(View.GONE);
+        mBinding.widget.top.setVisibility(View.GONE);
         mBinding.widget.center.setVisibility(View.GONE);
     }
 
