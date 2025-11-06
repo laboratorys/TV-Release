ffa515ac025b902c8c80d1eb210cd101c720a1a3
jhengazuki <jhengazuki3@gmail.com>
2025-11-06 19:43:53 +0800

    Revert

--- chaquo/src/main/java/com/fongmi/chaquo/Loader.java
+++ chaquo/src/main/java/com/fongmi/chaquo/Loader.java
@@ -1,21 +1,23 @@
 package com.fongmi.chaquo;
 
+import android.content.Context;
+
 import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
 import com.chaquo.python.android.AndroidPlatform;
-import com.github.catvod.Init;
 import com.github.catvod.utils.Path;
 
 public class Loader {
 
-    private final PyObject app;
+    private PyObject app;
 
-    public Loader() {
-        if (!Python.isStarted()) Python.start(new AndroidPlatform(Init.context()));
+    private void init(Context context) {
+        if (!Python.isStarted()) Python.start(new AndroidPlatform(context));
         app = Python.getInstance().getModule("app");
     }
 
-    public Spider spider(String api) {
+    public Spider spider(Context context, String api) {
+        if (app == null) init(context);
         PyObject obj = app.callAttr("spider", Path.py().getAbsolutePath(), api);
         return new Spider(app, obj, api);
     }
