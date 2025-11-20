906215b1374a33a51acbbe5379f49f9843f9dfa7
jhengazuji <jhengazuki3@gmail.com>
2025-11-20 15:22:16 +0800

    Fix bug

--- app/src/leanback/java/com/fongmi/android/tv/ui/activity/LiveActivity.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/activity/LiveActivity.java
@@ -586,7 +586,7 @@ public class LiveActivity extends BaseActivity implements GroupPresenter.OnClick
     }
 
     private void addKeep(Channel item) {
-        getKeep().add(item);
+        getKeep().keep(item);
         Keep keep = new Keep();
         keep.setKey(item.getName());
         keep.setType(1);
