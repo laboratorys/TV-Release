b7844e77cb5b22f46979acb86c54f90605abc5b2
jhengazuji <jhengazuki3@gmail.com>
2025-11-24 01:21:21 +0800

    Clean

--- app/src/mobile/java/com/fongmi/android/tv/ui/activity/VideoActivity.java
+++ app/src/mobile/java/com/fongmi/android/tv/ui/activity/VideoActivity.java
@@ -14,7 +14,6 @@ import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.TextUtils;
 import android.text.style.ClickableSpan;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
