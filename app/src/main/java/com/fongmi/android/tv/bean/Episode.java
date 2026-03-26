ca87566a186d710a98cb890c07eefd8a31a926bb
FongMi <b8892778@gmail.com>
2026-03-26 13:54:50 +0800

    Fix episode bug

--- app/src/main/java/com/fongmi/android/tv/bean/Episode.java
+++ app/src/main/java/com/fongmi/android/tv/bean/Episode.java
@@ -113,12 +113,12 @@ public class Episode implements Parcelable, Diffable<Episode> {
     public boolean equals(@Nullable Object obj) {
         if (this == obj) return true;
         if (!(obj instanceof Episode it)) return false;
-        return Objects.equals(getName(), it.getName());
+        return Objects.equals(getName(), it.getName()) && Objects.equals(getUrl(), it.getUrl());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getName());
+        return Objects.hash(getName(), getUrl());
     }
 
     @Override
