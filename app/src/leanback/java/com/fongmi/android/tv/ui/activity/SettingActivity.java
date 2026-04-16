79a5a2fad1fb829a14c62210bfdcd22076319054
laboratorys <359916450@qq.com>
2026-04-16 16:00:15 +0800

    feat: 重新整理后的纯净魔改

--- app/src/leanback/java/com/fongmi/android/tv/ui/activity/SettingActivity.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/activity/SettingActivity.java
@@ -23,6 +23,7 @@ import com.fongmi.android.tv.databinding.ActivitySettingBinding;
 import com.fongmi.android.tv.db.AppDatabase;
 import com.fongmi.android.tv.event.ConfigEvent;
 import com.fongmi.android.tv.event.RefreshEvent;
+import com.fongmi.android.tv.extra.Constant;
 import com.fongmi.android.tv.impl.Callback;
 import com.fongmi.android.tv.impl.ConfigCallback;
 import com.fongmi.android.tv.impl.DohCallback;
@@ -85,7 +86,7 @@ public class SettingActivity extends BaseActivity implements ConfigCallback, Sit
         mBinding.vodUrl.setText(VodConfig.getDesc());
         mBinding.liveUrl.setText(LiveConfig.getDesc());
         mBinding.wallUrl.setText(WallConfig.getDesc());
-        mBinding.versionText.setText(BuildConfig.VERSION_NAME);
+        mBinding.versionText.setText(String.format("%s-%s",BuildConfig.VERSION_NAME, Constant.VERSION_SUFFIX));
         setCacheText();
         setOtherText();
     }
@@ -107,7 +108,8 @@ public class SettingActivity extends BaseActivity implements ConfigCallback, Sit
 
     @Override
     protected void initEvent() {
-        mBinding.vod.setOnClickListener(this::onVod);
+        //mBinding.vod.setOnClickListener(this::onVod);
+        com.fongmi.android.tv.extra.DoubleClickListener.bind(mBinding.vod, this::onVod, v -> com.fongmi.android.tv.extra.BootConfig.loadDepot(getActivity()));
         mBinding.doh.setOnClickListener(this::setDoh);
         mBinding.live.setOnClickListener(this::onLive);
         mBinding.wall.setOnClickListener(this::onWall);
