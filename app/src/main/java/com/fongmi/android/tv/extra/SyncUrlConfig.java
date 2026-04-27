507fd02497ca866ee0007219b0604d9f72008856
laboratorys <359916450@qq.com>
2026-04-27 15:32:43 +0800

    feat: 适配tvbox-sync-server

---
+++ app/src/main/java/com/fongmi/android/tv/extra/SyncUrlConfig.java
@@ -0,0 +1,22 @@
+package com.fongmi.android.tv.extra;
+
+public class SyncUrlConfig {
+    private String baseUrl;
+    private String query;
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+}
